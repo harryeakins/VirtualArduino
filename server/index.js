95c721ac62f99b588125fd2e2f13a9f322f14a0d
Harry Eakins <harry.eakins@googlemail.com>
2014-01-11 22:09:59 +0000

    Started using winston for logging. Set logging level to info in socket.io

--- server/index.js
+++ server/index.js
@@ -4,15 +4,17 @@ var express = require('express')
 , server = require('http').createServer(app)
 , io = require('socket.io').listen(server)
 , spawn = require('child_process').spawn
-, path = require('path');
+, path = require('path')
+, log = require('winston');
 
 var PORT = 80;
 
+io.set('log level', 2); // info: 2, debug: 3
 
 app.use('/', express.static(__dirname + "/static"));
 
 server.listen(PORT);
-console.log("Server listening on " + PORT);
+log.info("Server listening on " + PORT);
 
 CODE_DIR = '/tmp';
 
@@ -21,19 +23,25 @@ io.sockets.on('connection', function (socket) {
 	socket.on('code', function(data) {
 		var code = data["code"];
 		var compiler = spawn('./run_docked_gdb.sh');
+        log.info("GDB Docker container started (PID: " + compiler.pid + ")");
 		socket.on('disconnect', function() {
-			if(compiler.connected) compiler.kill()
+            log.info("Socket disconnected. Killing process Docker container (PID: " + compiler.pid + ")");
+            compiler.kill()
 		});
+        compiler.on('close', function(code, signal) {
+            log.info("Process terminated. Code: " + code + " Signal: " + signal + " (PID: " + compiler.pid + ")");
+        });
 		compiler.stdout.setEncoding('utf8');
 		compiler.stderr.setEncoding('utf8');
 		var compiler_output = "";
 		compiler.stdout.on('data', function(data) {
-			console.log("stdout: " + data);
+			log.debug("compiler.stdout: " + data);
 			compiler_output += data;
 			if(data.trim() == "NOW_IN_GDB") {
+                log.info("Code successfully compiled (PID: " + compiler.pid + ")");
 				socket.emit('compiled', "Successful");
 				compiler.stdout.on('data', function(data) {
-					console.log("Received: " + data);
+					log.debug("compiler.stdout: " + data);
 					var lines = data.replace(/\n/g, "\n||").split("||")
 					for(var i = 0; i < lines.length; i++) {
 						if(lines[i].length != 0) {
@@ -42,18 +50,20 @@ io.sockets.on('connection', function (socket) {
 					}
 				});
 				compiler.stderr.on('data', function(data) {
+                    log.warn("compiler.stderr: " + data);
 					socket.emit('stderr', data);
 				});
 				socket.on('stdin', function(data) {
-					console.log("Received stdin: " + data)
+					log.debug("socket.stdin: " + data)
 					compiler.stdin.write(data);
 				});
 			}
 		});
 		compiler.stderr.on('data', function(data) {
-			console.log("stderr: " + data);
+			log.warn("compiler.stderr: " + data);
 			compiler_output += data;
 		});
+        log.info("Sending code to Docker container (PID: " + compiler.pid + ")");
 		compiler.stdin.write(code);
 		compiler.stdin.write("END_OF_ARDUINO_FILE\n");
 	});
