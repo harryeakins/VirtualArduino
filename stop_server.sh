8fa17700f7bbf33499663925bfbe4bd1ed67fe9a
Harry Eakins <harry.eakins@googlemail.com>
2014-01-11 21:24:03 +0000

    Modified stop_server.sh so that it kills running gdb docker containers

--- stop_server.sh
+++ stop_server.sh
@@ -1,6 +1,6 @@
 #! /bin/bash 
-set -e
 
+echo "Stopping server..."
 ./server/node_modules/forever/bin/forever stopall
-
-
+echo "Stopping gdb containers..."
+docker ps | grep gdb | cut -f1 -d" " | xargs docker kill
